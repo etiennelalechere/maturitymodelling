c0cecba67a8616f0a022d4485f1b83d316c42267
etiennelalechere <lalechereetienne@gmail.com>
2021-12-13 17:07:49 +0100

    predictMaturity

--- R/predictMaturity.R
+++ R/predictMaturity.R
@@ -14,32 +14,32 @@ predictMaturity=function(model,modelName,dir){
   names(metrics.map)[c(1,2,3,5,39,40,53,54,55,56,60,64,66,68)]=
     row.names(model$importance)[1:(length(row.names(model$importance)))]
   predictedIMAT_Chartreuse=raster::predict(metrics.map,model,progress="text",type="response")
-  raster::writeRaster(predictedIMAT_Chartreuse,filename = paste("./../data/outputs/predicted",modelName,"_Chartreuse.tif"),format="GTiff",prj=T,overwrite=T)
+  raster::writeRaster(predictedIMAT_Chartreuse,filename = paste("./../data/outputs/predicted",modelName,"_Chartreuse.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
   load(paste(dir,"metrics_allVercorsRBI.rda",sep=""))
   names(metrics.map)[c(1,2,3,5,39,40,53,54,55,56,60,64,66,68)]=
     row.names(protestModel_IMAT$importance)[1:(length(row.names(protestModel_IMAT$importance)))]
   predictedIMAT_VercorsRBI=raster::predict(metrics.map,protestModel_IMAT,progress="text",type="response")
-  raster::writeRaster(predictedIMAT_VercorsRBI,filename = paste("./../data/outputs/predicted",modelName,"_VercorsRBI.tif"),format="GTiff",prj=T,overwrite=T)
+  raster::writeRaster(predictedIMAT_VercorsRBI,filename = paste("./../data/outputs/predicted",modelName,"_VercorsRBI.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
   load(paste(dir,"metrics_allQuatreMontagnes.rda",sep=""))
   names(metrics.map)[c(1,2,3,5,39,40,53,54,55,56,60,64,66,68)]=
     row.names(protestModel_IMAT$importance)[1:(length(row.names(protestModel_IMAT$importance)))]
   predictedIMAT_QuatreMontagnes=raster::predict(metrics.map,protestModel_IMAT,progress="text",type="response")
-  raster::writeRaster(predictedIMAT_QuatreMontagnes,filename = paste("./../data/outputs/predicted",modelName,"_QuatreMontagnes.tif"),format="GTiff",prj=T,overwrite=T)
+  raster::writeRaster(predictedIMAT_QuatreMontagnes,filename = paste("./../data/outputs/predicted",modelName,"_QuatreMontagnes.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
 
   load(paste(dir,"metrics_allBugey.rda",sep=""))
   names(metrics.map)[c(1,2,3,5,39,40,53,54,55,56,60,64,66,68)]=
     row.names(protestModel_IMAT$importance)[1:(length(row.names(protestModel_IMAT$importance)))]
   predictedIMAT_Bugey=raster::predict(metrics.map,protestModel_IMAT,progress="text",type="response")
-  raster::writeRaster(predictedIMAT_Bugey,filename = paste("./../data/outputs/predicted",modelName,"_Bugey.tif"),format="GTiff",prj=T,overwrite=T)
+  raster::writeRaster(predictedIMAT_Bugey,filename = paste("./../data/outputs/predicted",modelName,"_Bugey.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
 
   load(paste(dir,"metrics_allBauges.rda",sep=""))
   names(metrics.map.pnr)[c(1,2,3,5,39,40,53,54,55,56,60,64,67,69)]=
     row.names(protestModel_IMAT$importance)[1:(length(row.names(protestModel_IMAT$importance)))]
   predictedIMAT_Bauges=raster::predict(metrics.map.pnr,protestModel_IMAT,progress="text",type="response")
-  raster::writeRaster(predictedIMAT_Bauges,filename = paste("./../data/outputs/predicted",modelName,"_Bauges.tif"),format="GTiff",prj=T,overwrite=T)
+  raster::writeRaster(predictedIMAT_Bauges,filename = paste("./../data/outputs/predicted",modelName,"_Bauges.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
 }
