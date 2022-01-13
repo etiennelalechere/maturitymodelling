eae6de9d20c0e76794e6750bb937f7e9e1510f3a
etiennelalechere <lalechereetienne@gmail.com>
2022-01-13 16:51:22 +0100

    predictMaturityforest

--- R/predictMaturityForest.R
+++ R/predictMaturityForest.R
@@ -8,27 +8,27 @@
 predictMaturityForest=function(modelName){
 
   mask=sf::st_read('./../data/outputs/forestBauges.shp')
-  predictedIMAT_Bauges=raster::raster("./../data/outputs/predictedIMAT_Bauges.tif")
+  predictedIMAT_Bauges=raster::raster(paste("./../data/outputs/predicted",modelName,"_Bauges.tif",sep=""))
   predictedIMAT_Bauges_forests=raster::mask(predictedIMAT_Bauges,mask)
   raster::writeRaster(predictedIMAT_Bauges_forests,filename = paste("./../data/outputs/predicted",modelName,"_Bauges_forest.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
   mask=sf::st_read('./../data/outputs/forestBugey.shp')
-  predictedIMAT_Bugey=raster::raster("./../data/outputs/predictedIMAT_Bugey.tif")
+  predictedIMAT_Bugey=raster::raster(paste("./../data/outputs/predicted",modelName,"_Bugey.tif",sep=""))
   predictedIMAT_Bugey_forests=raster::mask(predictedIMAT_Bugey,mask)
   raster::writeRaster(predictedIMAT_Bugey_forests,filename = paste("./../data/outputs/predicted",modelName,"_Bugey_forest.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
   mask=sf::st_read('./../data/outputs/forestChartreuse.shp')
-  predictedIMAT_Chartreuse=raster::raster("./../data/outputs/predictedIMAT_Chartreuse.tif")
+  predictedIMAT_Chartreuse=raster::raster(paste("./../data/outputs/predicted",modelName,"_Chartreuse.tif",sep=""))
   predictedIMAT_Chartreuse_forests=raster::mask(predictedIMAT_Chartreuse,mask)
   raster::writeRaster(predictedIMAT_Chartreuse_forests,filename = paste("./../data/outputs/predicted",modelName,"_Chartreuse_forest.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
   mask=sf::st_read('./../data/outputs/forestQuatreMontagnes.shp')
-  predictedIMAT_QuatreMontagnes=raster::raster("./../data/outputs/predictedIMAT_QuatreMontagnes.tif")
+  predictedIMAT_QuatreMontagnes=raster::raster(paste("./../data/outputs/predicted",modelName,"_QuatreMontagnes.tif",sep=""))
   predictedIMAT_QuatreMontagnes_forests=raster::mask(predictedIMAT_QuatreMontagnes,mask)
   raster::writeRaster(predictedIMAT_QuatreMontagnes_forests,filename = paste("./../data/outputs/predicted",modelName,"_QuatreMontagnes_forest.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 
   mask=sf::st_read('./../data/outputs/forestVercors.shp')
-  predictedIMAT_VercorsRBI=raster::raster("./../data/outputs/predictedIMAT_VercorsRBI.tif")
+  predictedIMAT_VercorsRBI=raster::raster(paste("./../data/outputs/predicted",modelName,"_VercorsRBI.tif",sep=""))
   predictedIMAT_VercorsRBI_forests=raster::mask(predictedIMAT_VercorsRBI,mask)
   raster::writeRaster(predictedIMAT_VercorsRBI_forests,filename = paste("./../data/outputs/predicted",modelName,"_VercorsRBI_forest.tif",sep=""),format="GTiff",prj=T,overwrite=T)
 }
